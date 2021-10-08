5648ff51ef6c8a2bf266f56294c06865a1733c5e
gdck <nicknill@vip.qq.com>
2021-10-09 01:25:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,8 +16,9 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #更改机器名
 #sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
-#添加软件源
+#添加软件源Lienol gdck
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git gdck https://github.com/gdck/openwrt-packages' feeds.conf.default
 # MTK闭源驱动
 sed -i '$a src-git mtk https://github.com/Nossiac/mtk-openwrt-feeds;lede-17.01' feeds.conf.default
 #mtk https://github.com/Nossiac/mtk-openwrt-feeds #;lede-17.01
@@ -34,10 +35,10 @@ scripts/feeds install -a -p mtk
 #=====================================================================================================
 #更改版本号为编译时间
 #sed -i 's/R20.10.20/R$(date +%Y.%m.%d.%H:%M)/g' package/lean/default-settings/files/zzz-default-settings
-sed -i "s/R2020.10.27.09:55/R$(date +%Y.%m.%d.%H:%M)/g" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/R20.10.20/R$(date +%Y.%m.%d.%H:%M)/g" package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/R2020.10.27.09:55/R$(date +%Y.%m.%d.%H:%M)/g" package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/R20.10.20/R$(date +%Y.%m.%d.%H:%M)/g" package/lean/default-settings/files/zzz-default-settings
 #=====================================================================================================
-#sed -i '46i 185.199.111.133 raw.githubusercontent.com' package/base-files/files/etc/hosts
+#sed -i '46i 199.232.28.133 raw.githubusercontent.com' package/base-files/files/etc/hosts
 #=====================================================================================================
 #本地启动脚本
 #启动脚本插入到 'exit 0' 之前即可随系统启动运行。
@@ -48,7 +49,7 @@ sed -i '4i /etc/init.d/samba disable' package/base-files/files/etc/rc.local #禁
 # 获取Lienol-package
 #git clone https://github.com/Lienol/openwrt-package package/diy-packages/lienol
 # 获取gdck-package
-git clone https://github.com/gdck/openwrt-packages package/gdck
+git clone https://github.com/gdck/openwrt-packages.git package/lean
 #================================================================================================================
 # 获取luci-app-diskman和依赖
 mkdir -p package/diy-packages/luci-app-diskman && \
@@ -59,109 +60,34 @@ wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Mak
 # 获取应用过滤luci-app-oaf
 #git clone https://github.com/destan19/OpenAppFilter package/lean/luci-app-oaf
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-# 获取luci-app-cupsd
-git clone https://github.com/gdck/luci-app-cupsd package/lean/luci-app-cupsd
 #==================================================================================================================
-# 获取hello world和依赖
-#git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
-#==========================================================================================================
-# 获取passwall
-#git clone -b 3.6-40 https://github.com/liuran001/luci-app-passwall package/diy-packages/passwall
-#svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall
-#git clone -b https://github.com/gdck/luci-app-passwall-plus package/lean/passwall
-#svn co https://github.com/gdck/openwrt-packages/trunk/luci-app-passwall package/lean/luci-app-passwall
-#svn co https://github.com/gdck/openwrt-packages/trunk/dns2socks package/lean/dns2socks
-#svn co https://github.com/gdck/openwrt-packages/trunk/ipt2socks package/lean/ipt2socks
-#svn co https://github.com/gdck/openwrt-packages/trunk/iptvhelper package/lean/iptvhelper
-#svn co https://github.com/gdck/openwrt-packages/trunk/microsocks package/lean/microsocks
-#svn co https://github.com/gdck/openwrt-packages/trunk/ssocks package/lean/ssocks
-#svn co https://github.com/gdck/openwrt-packages/trunk/tcping package/lean/tcping
-#svn co https://github.com/gdck/openwrt-packages/trunk/trojan-go package/lean/trojan-go
-#svn co https://github.com/gdck/openwrt-packages/trunk/trojan-plus package/lean/trojan-plus
-#svn co https://github.com/gdck/openwrt-packages/trunk/trojan package/lean/trojan
-#svn co https://github.com/gdck/openwrt-packages/trunk/v2ray-plugin package/lean/v2ray-plugin
-#svn co https://github.com/gdck/openwrt-packages/trunk/v2ray package/lean/v2ray
-#svn co https://github.com/gdck/openwrt-packages/trunk/naiveproxy package/lean/naiveproxy
-#svn co https://github.com/gdck/openwrt-packages/trunk/brook package/lean/brook
-#==========================================================================================================
-# 获取网络端口状态
-git clone https://github.com/gdck/luci-app-tn-netports.git package/lean/luci-app-tn-netports
-#svn co  https://github.com/gdck/luci-app-tn-netports package/lean/luci-app-tn-netports
-#git clone https://github.com/tano-systems/luci-app-tn-netports.git package/lean/luci-app-tn/netports
-#==========================================================================================================
-# 获取luci-app-qosv4
-#git clone https://github.com/gdck/luci-app-qosv4 package/lean/luci-app-qosv4
-#==========================================================================================================
-# 获取luci-app-speederv2#UDPspeeder辅助插件
-git clone https://github.com/halldong/luci-app-speederv2 package/lean/luci-app-speederv2
-#==========================================================================================================
-# 获取uci-app-radius Radius服务器
-#git clone https://github.com/MuJJus/luci-app-radius package/lean/luci-app-radius
-#==========================================================================================================
-# 获取luci-app-wifidog web认证界面
-#git clone https://github.com/gdck/luci-app-wifidog package/lean/luci-app-wifidog
-#==========================================================================================================
-# 获取通过arp实现的在线人员查看的luci界面，统计误差为arp老化时间
-git clone https://github.com/gdck/luci-app-onliner package/lean/luci-app-onliner
-#==========================================================================================================
-# 获取日志查看器
-git clone https://github.com/gdck/luci-app-tn-logview package/lean/luci-app-tn-logview
-#==========================================================================================================
-# 获取网易云音乐解锁mini
-#git clone https://github.com/gdck/luci-app-unblockneteasemusic-mini package/lean/luci-app-unblockneteasemusic-mini
-rm -rf package/lean/luci-app-unblockneteasemusic
-rm -rf package/lean/luci-app-unblockmusic
-#==========================================================================================================
-# 获取可道云 luci-app-kodexplorer
-#git clone https://github.com/silime/luci-app-kodexplorer package/lean/luci-app-kodexplorer
-#================================================================================================================
-# 获取luci-app-serverchan
-git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
-# 获取luci-app-control-weburl
-git clone https://github.com/gdck/luci-app-control-weburl package/lean/luci-app-control-weburl
-#=====================================================================================================================
-# 获取luci-app-smartdns
+# 获取luci-app-cupsd apple打印机
+git clone https://github.com/gdck/luci-app-cupsd.git package/lean/luci-app-cupsd
+#==================================================================================================================
+#获取luci-app-smartdnsDNS
 rm -rf package/lean/luci-app-smartdns
 svn co  https://github.com/gdck/openwrt-packages/trunk/luci-app-smartdns package/lean/luci-app-smartdns
 rm -rf package/lean/smartdns
 svn co https://github.com/gdck/openwrt-packages/trunk/smartdns package/lean/smartdns
-#=============================================================================================================================
-# 获取luci-app-chinadns-ng
-#svn co https://github.com/gdck/openwrt-packages/trunk/luci-app-chinadns-ng package/lean/luci-app-chinadns-ng
-#svn co https://github.com/gdck/openwrt-packages/trunk/chinadns-ng package/lean/chinadns-ng 
-#===============================================================================================================
-# 获取luci-app-adguardhome
-#pushd package/lean
-rm -rf package/lean/luci-app-adguardhome
-git clone https://github.com/gdck/luci-app-adguardhome package/lean/luci-app-adguardhome
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
 #============================================================================================================
-# 获取luci-app-ser2net串口网络透传
-svn co  https://github.com/gdck/openwrt-packages/trunk/luci-app-ser2net package/lean/luci-app-ser2net
-#==========================================================================================================
-#  获取luci-app-socat一个多功能的网络工具
-svn co  https://github.com/gdck/openwrt-packages/trunk/luci-app-socat package/lean/luci-app-socat
-#==========================================================================================================
+#获取luci-app-adguardhomeDNS
+#pushd package/lean
+#rm -rf package/lean/luci-app-adguardhome
+#git clone https://github.com/gdck/luci-app-adguardhome package/lean/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
+#======================================================================================================================
 # 获取luci-app-wrtbwmon
 #svn co https://github.com/gdck/openwrt-packages/branches/packages-19.07/luci-app-wrtbwmon package/lean/luci-app-wrtbwmon
 #svn co https://github.com/gdck/openwrt-packages/branches/packages-19.07/wrtbwmon package/lean/wrtbwmon
 #git clone https://github.com/kongfl888/luci-app-wrtbwmon package/lean/luci-app-wrtbwmon
-git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git package/lean/luci-app-wrtbwmon
-#======================================================================================================================
-# 获取luci-app-koolproxy
-#git clone https://github.com/gdck/luci-app-koolproxy package/lean/luci-app-koolproxy
-#======================================================================================================================
-# 获取luci-app-qos-gargoyle
-#git clone https://github.com/gdck/luci-app-qos-gargoyle package/lean/luci-app-qos-gargoyle
-#======================================================================================================================
-# 获取luci-app-openclash 编译po2lmo
-#git clone -b master https://github.com/vernesong/OpenClash package/diy-packages/openclash
-#pushd package/diy-packages/openclash/luci-app-openclash/tools/po2lmo
-#make && sudo make install
-#popd
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#git clone https://github.com/gdck/openwrt-packages.git package/gdck
+#git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git package/lean/luci-app-wrtbwmon
+#============================================================================================================
+# 获取luci-app-ser2net串口网络透传
+#svn co  https://github.com/gdck/openwrt-packages/trunk/luci-app-ser2net package/lean/luci-app-ser2net
+#==========================================================================================================
+#  获取luci-app-socat一个多功能的网络工具
+#svn co  https://github.com/gdck/openwrt-packages/trunk/luci-app-socat package/lean/luci-app-socat
+#==========================================================================================================
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # 清除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
@@ -170,12 +96,7 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 pushd package/lean
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+scripts/feeds update -a
+scripts/feeds install -a
 popd
 #===============================================================================================================================
-#===============================================================================================================================
-#pushd package/lean
-#rm -rf baidupcs-web
-#git clone https://github.com/liuran001/baidupcs-web-lede baidupcs-web
-#popd
-scripts/feeds update
-scripts/feeds install
