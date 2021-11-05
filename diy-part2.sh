e9dae32eb8a9b21415fa7fd419a1d1d74245dc53
gdck <nicknill@vip.qq.com>
2021-11-06 02:58:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,10 @@
 
 # Modify default IP 更改默认IP
 # Modify default IP
+#把config_generate文件中的192.168.1.1替换为192.168.50.5
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+#把tiny-tp-link.mk中227行的4替换成16
 sed -i '227s#4#16#' /target/linux/ath79/image/tiny-tp-link.mk
 
 #===================================================================================
